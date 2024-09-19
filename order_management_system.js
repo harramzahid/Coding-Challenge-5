fc93ef4f6f1f6959c302b41718cdb437439278e0
harramzahid <harramzahid5@gmail.com>
2024-09-19 14:41:07 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,21 @@
+//TASK 1 Create an Inventory Array of Product Objects
+const inventory = [
+
+    { name: 'Espresso', price: 3, quantity: 10 },  
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+    
+    ];
+
+// console.log(inventory);
+// here, for task one I have innitialized an inventory
+
+// Task 2 Create an Orders Array of Order Objects
+let objects = [
+    { name: 'Espresso', price: 3, quantity: 10 },  
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+    
+]
