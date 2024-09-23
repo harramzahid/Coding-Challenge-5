b24049e8e9d01dac6ad1fc54d15cfd47706de07c
harramzahid <harramzahid5@gmail.com>
2024-09-22 21:32:47 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -12,10 +12,17 @@ const inventory = [
 // here, for task one I have innitialized an inventory
 
 // Task 2 Create an Orders Array of Order Objects
-let objects = [
-    { name: 'Espresso', price: 3, quantity: 10 },  
-    { name: 'Latte', price: 4, quantity: 5 },
-    { name: 'Cappuccino', price: 4, quantity: 6 },
-    { name: 'Mocha', price: 5, quantity: 4 }
-    
-]
+let orders = [];
+
+// Example of an order object to be added to the orders array
+let order = {
+    customerName: "Mia",
+    items: [
+        { productName: "Mocha", quantity: 1 },
+        { productName: "Latte", quantity: 2 }
+    ],
+    status: "Pending"
+};
+
+// Adding the example order to the orders array
+orders.push(order);
